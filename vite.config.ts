0f0b658cfbb9bebed82d598d51571eb7b8470e47
taysouzaa <taynara.souza.dev@gmail.com>
2026-03-05 13:50:51 -0300

    Build with ads.html entry

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,14 @@ export default defineConfig({
     },
   },
 
+  build: {
+    rollupOptions: {
+      input: {
+        ads: path.resolve(__dirname, 'ads.html'),
+      },
+    },
+  },
+
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
